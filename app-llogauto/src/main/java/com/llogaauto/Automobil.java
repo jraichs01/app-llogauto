83e8246128ff21cb783a17c8debabe9ab74ce528
jraichs01 <jraichs@inscaparrella.cat>
2026-03-24 16:36:27 +0100

    " tot fet, falten les proves JUNIT"

--- app-llogauto/src/main/java/com/llogaauto/Automobil.java
+++ app-llogauto/src/main/java/com/llogaauto/Automobil.java
@@ -7,16 +7,22 @@ public class Automobil {
     String matricula;
 
     // mètodes
-    Automobil(String mar, String mod, String mat){
+    Automobil(String mar, String mod, String mat) {
         this.marca = mar;
         this.model = mod;
         this.matricula = mat;
     }
 
-    public String mostrarDades(){
+    public String mostrarDades() {
         return " marca : " + this.marca + "   model: " + this.model + "  matricula:" + this.matricula;
     }
 
+    public static boolean validarMatricula(String matricula) {
 
-    
+        // Regex: 4 dígits + 3 lletres majúscules (excloent vocals segons l'estàndard i
+        // el test)
+        return matricula != null && matricula.matches("^[0-9]{4}[BCDFGHJKLMNPQRSTVWXYZ]{3}$");
+
+    }
+
 }
