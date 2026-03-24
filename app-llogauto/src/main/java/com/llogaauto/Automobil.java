0da99398cca4b61a328ff3bb192155e4094f785d
jraichs01 <jraichs@inscaparrella.cat>
2026-03-24 16:31:14 +0100

    "v4"

--- app-llogauto/src/main/java/com/llogaauto/Automobil.java
+++ app-llogauto/src/main/java/com/llogaauto/Automobil.java
@@ -1,14 +1,22 @@
 package com.llogaauto;
 
 public class Automobil {
+    // Atributs
     String marca;
     String model;
     String matricula;
 
+    // mètodes
     Automobil(String mar, String mod, String mat){
         this.marca = mar;
         this.model = mod;
         this.matricula = mat;
     }
+
+    public String mostrarDades(){
+        return " marca : " + this.marca + "   model: " + this.model + "  matricula:" + this.matricula;
+    }
+
+
     
 }
