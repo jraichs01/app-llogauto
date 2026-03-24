cc4cd8bf2300ee581c6a793a07dc7d2e84c38498
jraichs01 <jraichs@inscaparrella.cat>
2026-03-24 16:26:07 +0100

    "varsio 1"

---
+++ app-llogauto/src/main/java/com/llogaauto/Automobil.java
@@ -0,0 +1,14 @@
+package com.llogaauto;
+
+public class Automobil {
+    String marca;
+    String model;
+    String matricula;
+
+    Automobil(String mar, String mod, String mat){
+        this.marca = mar;
+        this.mod = model;
+        this.matricula = mat;
+    }
+    
+}
