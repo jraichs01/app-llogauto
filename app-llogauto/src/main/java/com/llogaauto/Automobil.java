ccb441758d6538fd6528375fe3e98a49ef8c4b41
jraichs01 <jraichs@inscaparrella.cat>
2026-03-25 19:07:28 +0100

    " Versió de 25.3.2026 - en principi correcta"

--- app-llogauto/src/main/java/com/llogaauto/Automobil.java
+++ app-llogauto/src/main/java/com/llogaauto/Automobil.java
@@ -5,18 +5,31 @@ public class Automobil {
     String marca;
     String model;
     String matricula;
+    
+    // Variable de classe
+    public static int comptador = 0;
 
-    // mètodes
+    // constructors
     Automobil(String mar, String mod, String mat) {
         this.marca = mar;
         this.model = mod;
         this.matricula = mat;
+        comptador ++;
     }
 
+    Automobil(){
+        this.marca = "";
+        this.model = "";
+        this.matricula = "";
+        comptador --;
+    }
+
+    // metode de la classe
     public String mostrarDades() {
         return " marca : " + this.marca + "   model: " + this.model + "  matricula:" + this.matricula;
     }
 
+    // funció de la classe
     public static boolean validarMatricula(String matricula) {
 
         // Regex: 4 dígits + 3 lletres majúscules (excloent vocals segons l'estàndard i
--- app-llogauto/src/main/java/com/llogaauto/Main.java
+++ app-llogauto/src/main/java/com/llogaauto/Main.java
@@ -2,6 +2,14 @@ package com.llogaauto;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        Automobil auto1 = new Automobil("Seat", "Ibiza", "1234HJK");
+        System.out.println(auto1.mostrarDades());
+        if(Automobil.validarMatricula(auto1.matricula)){
+            System.out.println(" El cotxe " + auto1.marca + "  " + auto1.model + " amb matricula " + auto1.matricula + " BEN MATRICULAT");
+        } else {
+             System.out.println(" El cotxe " + auto1.marca + "  " + auto1.model + " amb matricula " + auto1.matricula + " MATRICULA INCORRECTA ");
+        }
+
     }
 }
