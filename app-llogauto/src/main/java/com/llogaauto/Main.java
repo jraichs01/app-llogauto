5aeb8f7c2ce05f26b3da044a76f9c432a87e31ab
jraichs01 <jraichs@inscaparrella.cat>
2026-03-25 19:48:38 +0100

    " versió final Activitat 5.1"

--- app-llogauto/src/main/java/com/llogaauto/Main.java
+++ app-llogauto/src/main/java/com/llogaauto/Main.java
@@ -2,13 +2,16 @@ package com.llogaauto;
 
 public class Main {
     public static void main(String[] args) {
-        
+
         Automobil auto1 = new Automobil("Seat", "Ibiza", "1234HJK");
         System.out.println(auto1.mostrarDades());
-        if(Automobil.validarMatricula(auto1.matricula)){
-            System.out.println(" El cotxe " + auto1.marca + "  " + auto1.model + " amb matricula " + auto1.matricula + " BEN MATRICULAT");
+        
+        if (Automobil.validarMatricula(auto1.matricula)) {
+            System.out.println(" El cotxe " + auto1.marca + "  " + auto1.model + " amb matricula " + auto1.matricula
+                    + " BEN MATRICULAT");
         } else {
-             System.out.println(" El cotxe " + auto1.marca + "  " + auto1.model + " amb matricula " + auto1.matricula + " MATRICULA INCORRECTA ");
+            System.out.println(" El cotxe " + auto1.marca + "  " + auto1.model + " amb matricula " + auto1.matricula
+                    + " MATRICULA INCORRECTA ");
         }
 
     }
