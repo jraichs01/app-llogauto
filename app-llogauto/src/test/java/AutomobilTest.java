03def64d2d1fd461ed480e68295af2a0d2db3e9a
jraichs01 <jraichs@inscaparrella.cat>
2026-04-22 19:05:46 +0200

    " Fi del projecte UML_gestorautomobil"

--- app-llogauto/src/test/java/AutomobilTest.java
+++ app-llogauto/src/test/java/AutomobilTest.java
@@ -1,19 +1,20 @@
 import com.llogaauto.*;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-
-class MatriculaTest {
-
-
+// Classe de test per a la classe Automobil
+public class AutomobilTest {
+    
+    // validem el format de la matrícula amb diferents casos de prova
+    // casos vàlids
     @Test
     void testMatriculaValida() {
         assertTrue(Automobil.validarMatricula("1234LMN"));
         assertTrue(Automobil.validarMatricula("0000BBB"));
     }
 
-
+    // casos invàlids
     @Test
     void testMatriculaInvalida() {
         assertFalse(Automobil.validarMatricula("1234ABC")); // Té vocals (A)
@@ -21,4 +22,16 @@ class MatriculaTest {
         assertFalse(Automobil.validarMatricula("123BBB"));  // Falta un número
         assertFalse(Automobil.validarMatricula("1234BÑB")); // Lletra no permesa (Ñ)
     }
+
+    // casos límit
+    @Test
+    void testValidarMatriculaNull() {
+        assertFalse(Automobil.validarMatricula(null));
+    }
+
+    @Test
+    void testValidarMatriculaEmpty() {
+        assertFalse(Automobil.validarMatricula(""));
+    }
 }
+
